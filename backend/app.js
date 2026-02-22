bce3d1c7464ad7e7a4936f0ebb24fc5033f5e8cc
alainejoseph <alainejoseph2004@gmail.com>
2026-02-22 11:26:08 +0530

    phase 1

--- backend/app.js
+++ backend/app.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: ["http://localhost:5173", "https://sgf-pi.vercel.app"],
   credentials: true
 }))
 app.use(session({
